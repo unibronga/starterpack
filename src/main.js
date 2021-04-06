937c65b2cb1c08da64f2644567b5f666d01d2ca5
Konstantine <icon.dnepr@gmail.com>
2021-04-06 18:04:07 +0300

    add rxJS

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
 import './style/scss/style.scss';
 import '.././babel.config.json';
+import { Observable } from 'rxjs';
+
+const source = Observable.create((observer) => {
+    let count = 0;
+
+    console.log('Observable created');
+
+    const timer = setInterval(() => {
+        console.log('observer = ', observer.next(count))
+        observer.next(count);
+        count++;
+    }, 1000);
+
+    return () => {
+        console.log('Observable destroyed');
+        clearInterval(timer);
+    }
+});
 
 
-console.log('StarterPack running')
+const subscription = source.subscribe(
+    val => console.log('next:', val),
+    err => console.error('error:', err),
+    () => console.log('completed')
+);
+
+// не забываем отписаваться
+setTimeout(() => subscription.unsubscribe(), 4500);
+
+
+
+
+
